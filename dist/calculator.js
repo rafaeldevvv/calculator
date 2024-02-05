4477607310e732c2b051424ff6ee5200bddbf367
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-02-05 18:39:49 -0300

    refactor(ts): get calculator elements through calculator node

--- dist/calculator.js
+++ dist/calculator.js
@@ -5,7 +5,7 @@ function updateCalculatorExpression(exp) {
     expressionNode.textContent = exp;
     expressionNode.scrollLeft = expressionNode.scrollWidth;
 }
-const symbolKeys = document.querySelectorAll("[data-symbol]"), resetKey = document.querySelector(".js-reset-key"), delKey = document.querySelector(".js-del-key"), resultKey = document.querySelector(".js-result-key");
+const calculator = document.querySelector(".calculator"), symbolKeys = calculator.querySelectorAll("[data-symbol]"), resetKey = calculator.querySelector(".js-reset-key"), delKey = calculator.querySelector(".js-del-key"), resultKey = calculator.querySelector(".js-result-key");
 symbolKeys.forEach((k) => {
     const symbol = k.getAttribute("data-symbol");
     k.addEventListener("click", () => {
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,19 +1,20 @@
 let expression = "";
 
-const expressionNode = document.querySelector(".js-expression") as HTMLElement;
+const calculator = document.querySelector(".calculator") as HTMLElement,
+  /* 
+    all nodes that have a data-symbol attribute 
+    are used to add a symbol to the expression 
+  */
+  symbolKeys = calculator.querySelectorAll("[data-symbol]"),
+  resetKey = calculator.querySelector(".js-reset-key") as HTMLButtonElement,
+  delKey = calculator.querySelector(".js-del-key") as HTMLButtonElement,
+  resultKey = calculator.querySelector(".js-result-key") as HTMLButtonElement,
+  expressionNode = calculator.querySelector(".js-expression") as HTMLElement;
+  
 function updateCalculatorExpression(exp: string) {
   expressionNode.textContent = exp;
   expressionNode.scrollLeft = expressionNode.scrollWidth;
 }
-
-/* 
-   all nodes that have a data-symbol attribute 
-   are used to add a symbol to the expression 
-*/
-const symbolKeys = document.querySelectorAll("[data-symbol]"),
-  resetKey = document.querySelector(".js-reset-key") as HTMLButtonElement,
-  delKey = document.querySelector(".js-del-key") as HTMLButtonElement,
-  resultKey = document.querySelector(".js-result-key") as HTMLButtonElement;
 
 symbolKeys.forEach((k) => {
   const symbol = k.getAttribute("data-symbol");
