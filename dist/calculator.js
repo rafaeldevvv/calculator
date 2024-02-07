62f0e64a41780439223cc3d0a4769bf9ffb4fa56
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-02-07 16:08:42 -0300

    feat: implement calculation functionality

--- dist/calculator.js
+++ dist/calculator.js
@@ -25,3 +25,91 @@ window.addEventListener("keydown", (e) => {
     if (e.key === "Backspace")
         deleteLastCharacter();
 });
+resultKey.addEventListener("click", getResult);
+function getResult() {
+    if (expression.length === 0)
+        return;
+    try {
+        const result = doTheMath(expression);
+        expression = result.toString();
+        updateCalculatorExpression(expression);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+const operators = ["+", "-", "x", "÷"];
+const precedence = [
+    ["x", "÷"],
+    ["+", "-"],
+];
+const operatorsFunctions = {
+    x: (a, b) => a * b,
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "÷": (a, b) => a / b,
+};
+const number = /([-+]?\d+\.\d+)|([-+]?\d+\.?)|([-+]?\.?\d+)/, onlyNumber = /^(([-+]?\d+\.\d+)|([-+]?\d+\.?)|([-+]?\.?\d+))$/;
+function expRegExp(operatorCharacterClass) {
+    return new RegExp(`(${number.source})(${operatorCharacterClass})(${number.source})`);
+}
+const firstExps = expRegExp("[x÷]"), secondExps = expRegExp("[-+]");
+function destructureExpression(exp) {
+    const firstTermMatch = number.exec(exp), term1 = firstTermMatch[0], rest = exp.slice(term1.length), operator = rest[0], term2 = rest.slice(1);
+    return [Number(term1), operator, Number(term2)];
+}
+function getSign(n) {
+    return n < 0 ? "-" : "+";
+}
+function doTheMath(exp) {
+    checkValidity(exp);
+    if (onlyNumber.test(exp)) {
+        return Number(exp);
+    }
+    exp = solveExpressions(exp, firstExps);
+    exp = solveExpressions(exp, secondExps);
+    return Number(exp);
+}
+function solveExpressions(exp, targetExpressionRegExp) {
+    while (exp.search(targetExpressionRegExp) !== -1) {
+        const match = targetExpressionRegExp.exec(exp);
+        if (!match)
+            break;
+        const expression = match[0], { index } = match;
+        const [term1, operator, term2] = destructureExpression(expression);
+        const operatorFunc = operatorsFunctions[operator], result = operatorFunc(term1, term2);
+        if (exp.length === expression.length) {
+            exp = result.toString();
+            continue;
+        }
+        let before = exp.substring(0, index), after = exp.substring(index + expression.length);
+        if (before !== "" && result >= 0) {
+            before += "+";
+        }
+        exp = before + result + after;
+    }
+    return exp;
+}
+const tooBigNumber = /(\d{16,})/, multipleOperators = /([-+÷x]{2,})/, multipleDots = /(\.{2,})/, missingOperand = /(\d+[-+÷x])$/, invalidDot = /\D\.\D/, invalidDotAlone = /^(\.|\.\D|\D\.)$/;
+function checkValidity(exp) {
+    const tooBigNumberMatch = tooBigNumber.exec(exp);
+    if (tooBigNumberMatch) {
+        throw new RangeError(`Number is too big: '${tooBigNumberMatch[1]}'`);
+    }
+    const multipleOperatorsMatch = multipleOperators.exec(exp);
+    if (multipleOperatorsMatch) {
+        throw new SyntaxError(`You cannot put multiple operators together: '${multipleOperatorsMatch[1]}'`);
+    }
+    const multipleDotsMatch = multipleDots.exec(exp);
+    if (multipleDotsMatch) {
+        throw new SyntaxError(`Invalid expression: '${multipleDotsMatch[1]}'`);
+    }
+    const missingOperandMatch = missingOperand.exec(exp);
+    if (missingOperandMatch) {
+        throw new SyntaxError(`You forgot a number in '${missingOperandMatch[0]}'`);
+    }
+    const invalidDotMatch = invalidDot.exec(exp) || invalidDotAlone.exec(exp);
+    if (invalidDotMatch) {
+        throw new SyntaxError(`Invalid dot: '${invalidDotMatch[0]}'`);
+    }
+}
--- src/calculator.ts
+++ src/calculator.ts
@@ -39,3 +39,168 @@ delKey.addEventListener("click", deleteLastCharacter);
 window.addEventListener("keydown", (e) => {
   if (e.key === "Backspace") deleteLastCharacter();
 });
+
+resultKey.addEventListener("click", getResult);
+
+function getResult() {
+  if (expression.length === 0) return;
+  try {
+    const result = doTheMath(expression);
+    expression = result.toString();
+    updateCalculatorExpression(expression);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const operators = ["+", "-", "x", "÷"] as const;
+type Operator = (typeof operators)[number];
+
+const precedence = [
+  ["x", "÷"],
+  ["+", "-"],
+] as const;
+
+const operatorsFunctions = {
+  x: (a: number, b: number) => a * b,
+  "+": (a: number, b: number) => a + b,
+  "-": (a: number, b: number) => a - b,
+  "÷": (a: number, b: number) => a / b,
+} as const;
+
+const number = /([-+]?\d+\.\d+)|([-+]?\d+\.?)|([-+]?\.?\d+)/,
+  onlyNumber = /^(([-+]?\d+\.\d+)|([-+]?\d+\.?)|([-+]?\.?\d+))$/;
+
+/**
+ * Builds a mathematical expression regular expression.
+ * The regular expression will look like `/(term1)(operator)(term2)/`.
+ *
+ * @param operatorCharacterClass - A character class for operators, such as '[-+]'.
+ * @returns - A mathematical expression regular expression. This regular expression
+ * holds three sequential groups, which are, respectively, the first term, the operator
+ * used and the second term.
+ *
+ * @example
+ * const regexp = expRegExp("[+-]");
+ * console.log(regexp.source);
+ * // -> ((\d+\.?|\.?\d+|\d+\.\d+)([+-])(\d+\.?|\.?\d+|\d+\.\d+))
+ */
+function expRegExp(operatorCharacterClass: string) {
+  return new RegExp(
+    `(${number.source})(${operatorCharacterClass})(${number.source})`
+  );
+}
+
+const firstExps = expRegExp("[x÷]"),
+  secondExps = expRegExp("[-+]");
+
+/**
+ * Destructures a simple mathematical expression like 1+5.
+ *
+ * @param exp
+ * @returns - A tuple type whose elements are, respectively, the first term, the operator
+ * and the second term.
+ */
+function destructureExpression(
+  exp: string
+): [term1: number, operator: string, term2: number] {
+  const firstTermMatch = number.exec(exp)!,
+    term1 = firstTermMatch[0],
+    rest = exp.slice(term1.length),
+    operator = rest[0],
+    term2 = rest.slice(1);
+
+  return [Number(term1), operator, Number(term2)];
+}
+
+function getSign(n: number): "+" | "-" {
+  return n < 0 ? "-" : "+";
+}
+
+function doTheMath(exp: string): number {
+  checkValidity(exp);
+
+  if (onlyNumber.test(exp)) {
+    return Number(exp);
+  }
+
+  exp = solveExpressions(exp, firstExps);
+  exp = solveExpressions(exp, secondExps);
+
+  return Number(exp);
+}
+
+/**
+ * Solves expression in a bigger expression based on the regular expression
+ * passed in.
+ *
+ * @param exp - The whole expression.
+ * @param targetExpressionRegExp - The regular expressions that matches the
+ * smaller expressions that need to be solved.
+ * @returns - An expression with the required expressions solved.
+ *
+ * For example, suppose `multiplicationRegExp` is an expression that matches multiplications expressions like 8x5.
+ * @example
+ * const additions = solveExpressions("1+1+2x7", multiplicationRegExp);
+ * // -> additions is "1+2+14"
+ */
+function solveExpressions(exp: string, targetExpressionRegExp: RegExp) {
+  while (exp.search(targetExpressionRegExp) !== -1) {
+    const match = targetExpressionRegExp.exec(exp)!;
+    if (!match) break;
+    const expression = match[0],
+      { index } = match;
+    const [term1, operator, term2] = destructureExpression(expression);
+
+    const operatorFunc = operatorsFunctions[operator as Operator],
+      result = operatorFunc(term1, term2);
+    if (exp.length === expression.length) {
+      exp = result.toString();
+      continue;
+    }
+    let before = exp.substring(0, index),
+      after = exp.substring(index + expression.length);
+    if (before !== "" && result >= 0) {
+      before += "+";
+    }
+    exp = before + result + after;
+  }
+
+  return exp;
+}
+
+const tooBigNumber = /(\d{16,})/,
+  multipleOperators = /([-+÷x]{2,})/,
+  multipleDots = /(\.{2,})/,
+  missingOperand = /(\d+[-+÷x])$/,
+  invalidDot = /\D\.\D/,
+  invalidDotAlone = /^(\.|\.\D|\D\.)$/;
+
+function checkValidity(exp: string) {
+  const tooBigNumberMatch = tooBigNumber.exec(exp);
+  if (tooBigNumberMatch) {
+    throw new RangeError(`Number is too big: '${tooBigNumberMatch[1]}'`);
+  }
+
+  const multipleOperatorsMatch = multipleOperators.exec(exp);
+  if (multipleOperatorsMatch) {
+    throw new SyntaxError(
+      `You cannot put multiple operators together: '${multipleOperatorsMatch[1]}'`
+    );
+  }
+
+  const multipleDotsMatch = multipleDots.exec(exp);
+  if (multipleDotsMatch) {
+    throw new SyntaxError(`Invalid expression: '${multipleDotsMatch[1]}'`);
+  }
+
+  const missingOperandMatch = missingOperand.exec(exp);
+  if (missingOperandMatch) {
+    throw new SyntaxError(`You forgot a number in '${missingOperandMatch[0]}'`);
+  }
+
+  const invalidDotMatch = invalidDot.exec(exp) || invalidDotAlone.exec(exp);
+  if (invalidDotMatch) {
+    throw new SyntaxError(`Invalid dot: '${invalidDotMatch[0]}'`);
+  }
+}
