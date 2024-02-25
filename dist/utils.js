3612cc16eca21c512728667d0eaae2090409124c
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-02-25 16:20:12 -0300

    fix: format big numbers correctly

--- dist/utils.js
+++ dist/utils.js
@@ -24,7 +24,29 @@ export function splitAtIndex(str, index, includeCharacterAtIndex = true) {
 }
 export function formatNumbers(exp) {
     return exp.replace(/([^.\d]|^)(\d+)/g, (_, chBefore, number) => {
-        const n = Number(number);
-        return (chBefore || "") + n.toLocaleString("en");
+        return (chBefore || "") + addChBetween(number, 3, ",", true);
     });
+}
+export function addChBetween(str, step, ch, fromEnd) {
+    if (step < 0) {
+        throw new RangeError(`Invalid step: ${step}. Step must be zero or bigger.`);
+    }
+    let newStr = "", chCount = 0;
+    if (fromEnd) {
+        for (let i = str.length - 1; i >= 0; i--) {
+            chCount++;
+            newStr = str[i] + newStr;
+            if (chCount % step === 0 && i !== 0)
+                newStr = ch + newStr;
+        }
+    }
+    else {
+        for (let i = 0; i < str.length; i++) {
+            chCount++;
+            newStr += str[i];
+            if (chCount % step === 0 && i !== str.length - 1)
+                newStr += ch;
+        }
+    }
+    return newStr;
 }
--- src/utils.ts
+++ src/utils.ts
@@ -79,7 +79,44 @@ export function splitAtIndex(
  */
 export function formatNumbers(exp: string) {
   return exp.replace(/([^.\d]|^)(\d+)/g, (_, chBefore, number) => {
-    const n = Number(number);
-    return (chBefore || "") + n.toLocaleString("en");
+    return (chBefore || "") + addChBetween(number, 3, ",", true);
   });
+}
+
+/**
+ * Adds a string between characters in a string depending on the step passed in.
+ *
+ * @param str A string to add the other string between the characters.
+ * @param step How many characters should be skipped to add the specified string.
+ * @param ch The string you want to add between characters in a string.
+ * @param fromEnd Whether or not the string should be added from the end of the string.
+ * @throws If the step is lower than 0.
+ */
+export function addChBetween(
+  str: string,
+  step: number,
+  ch: string,
+  fromEnd?: boolean
+) {
+  if (step < 0) {
+    throw new RangeError(`Invalid step: ${step}. Step must be zero or bigger.`);
+  }
+
+  let newStr = "",
+    chCount = 0;
+  if (fromEnd) {
+    for (let i = str.length - 1; i >= 0; i--) {
+      chCount++;
+      newStr = str[i] + newStr;
+      if (chCount % step === 0 && i !== 0) newStr = ch + newStr;
+    }
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      chCount++;
+      newStr += str[i];
+      if (chCount % step === 0 && i !== str.length - 1) newStr += ch;
+    }
+  }
+
+  return newStr;
 }
