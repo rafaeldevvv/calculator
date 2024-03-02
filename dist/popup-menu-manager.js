23fd37321115c294198546f753312c22356a9725
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-03-02 18:52:00 -0300

    fix: position the history popover correctly

--- dist/popup-menu-manager.js
+++ dist/popup-menu-manager.js
@@ -1,13 +1,15 @@
-function positionPopover(popover, positionTarget) {
-    const { top, left } = positionTarget.getBoundingClientRect();
+function positionPopover(popover, positionTargetElement) {
+    const { top, left } = positionTargetElement.getBoundingClientRect();
     popover.style.left = left + "px";
-    popover.style.top = top + 10 + positionTarget.clientHeight + "px";
+    popover.style.top = top + 10 + positionTargetElement.clientHeight + window.scrollY + "px";
 }
 export default function manageHistoryPopover(popoverToggle) {
     const popoverId = popoverToggle.getAttribute("popovertarget");
     const popover = document.getElementById(popoverId);
     positionPopover(popover, popoverToggle);
     window.addEventListener("resize", () => {
-        positionPopover(popover, popoverToggle);
+        if (popover.matches(":popover-open")) {
+            positionPopover(popover, popoverToggle);
+        }
     });
 }
--- src/popup-menu-manager.ts
+++ src/popup-menu-manager.ts
@@ -1,15 +1,20 @@
-function positionPopover(popover: HTMLElement, positionTarget: HTMLElement) {
-  const { top, left } = positionTarget.getBoundingClientRect();
+function positionPopover(
+  popover: HTMLElement,
+  positionTargetElement: HTMLElement
+) {
+  const { top, left } = positionTargetElement.getBoundingClientRect();
   popover.style.left = left + "px";
-  popover.style.top = top + 10 + positionTarget.clientHeight + "px";
+  popover.style.top = top + 10 + positionTargetElement.clientHeight + window.scrollY + "px";
 }
 
 export default function manageHistoryPopover(popoverToggle: HTMLElement) {
   const popoverId = popoverToggle.getAttribute("popovertarget") as string;
-  
+
   const popover = document.getElementById(popoverId) as HTMLElement;
   positionPopover(popover, popoverToggle);
   window.addEventListener("resize", () => {
-    positionPopover(popover, popoverToggle);
+    if (popover.matches(":popover-open")) {
+      positionPopover(popover, popoverToggle);
+    }
   });
 }
